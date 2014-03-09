2c0a93611f4c0db6bb6ce362f48db0b0bcdb6a82
neil patel <neil@myindianweddingapp.com>
2014-03-09 22:07:23 +0000

    first commit

---
+++ app/models/listing.rb
@@ -0,0 +1,4 @@
+class Listing < ActiveRecord::Base
+  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg"
+  validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Payyolademo::Application.config.secret_key_base = '0a516bd469e1c6e477fc5689ded3a262a494571488d2aacf36628b4f0caab7285552c5986f7ca8df4a712fdd8ec6213221b12c48d579c830f751f7c950486474'
