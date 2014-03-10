b8389a3a67484affe6565414026336b5afcf8de7
neil patel <neil@myindianweddingapp.com>
2014-03-10 22:18:30 +0000

    Finish setup for Dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,11 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg"
-  validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png),
-   								    :storage => :dropbox,
-  					            	:dropbox_credentials => Rails.root.join("config/dropbox.yml")
-  
+	if Rails.env.development?
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg"
+		validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
+	else
+	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg",
+					  :storage => :dropbox,
+    				  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    				  :path => ":style/id_:filename"
+    end 
 end
