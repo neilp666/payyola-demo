0fefd2b38cb03c811c694e14f966eee012fdedb5
neil patel <neil@myindianweddingapp.com>
2014-03-11 19:54:28 +0000

    changed validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,11 +1,14 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg"
-		validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
 	else
 	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg",
+
 					  :storage => :dropbox,
     				  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     				  :path => ":style/id_:filename"
     end 
+     validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
+		
+
 end
